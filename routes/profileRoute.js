acc58638b1e086f42c6f418062203f22a3a97602
Praveen Sharma <sharmapraveenofficial@gmail.com>
2020-07-20 11:30:21 +0530

    back-end

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -2,68 +2,88 @@ const router = require("express").Router();
 const bookmarks = require("../model/userBookmark");
 
 const authCheck = (req, res, next) => {
-    if (!req.user) {
-        res.redirect("/auth/google");
-    } else {
-        next();
-    }
+  if (!req.user) {
+    res.redirect("/auth/google");
+  } else {
+    next();
+  }
 };
 
 router.get("/", authCheck, (req, res, next) => {
-    try {
-        bookmarks.findOne({
-            email: req.user.email
-        }).then(currentUser => {
-            let entries = [];
-            if (currentUser !== null) {
-                let obj = JSON.parse(currentUser.data);
-                Object.entries(obj).forEach(entry => {
-                    if (entry[1].liked == true) {
-                        if (entry[1].hashtag == "") {
-                            entry[1].hashtag = "#Bookmark"
-                        }
-                        let str = entry[0].split(".");
-                        if (str[0] !== "www") {
-                            entry[1].similarWebsite = "www." + entry[0];
-                        } else {
-                            entry[1].similarWebsite = entry[0];
-                        }
-                        let time_ = entry[1].trackedSeconds;
-                        let timer = secondsToHms(time_);
+  try {
+    bookmarks
+      .findOne({
+        email: req.user.email
+      })
+      .then(currentUser => {
+        let entries = [];
+        if (currentUser !== null) {
+          let obj = JSON.parse(currentUser.data);
+          Object.entries(obj).forEach(entry => {
+            if (entry[1].liked == true) {
+              if (entry[1].hashtag == "") {
+                entry[1].hashtag = "#Bookmark";
+              }
 
-                        function secondsToHms(d) {
-                            d = Number(d);
-                            var h = Math.floor(d / 3600);
-                            var m = Math.floor((d % 3600) / 60);
-                            var s = Math.floor((d % 3600) % 60);
-                            var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-                            var mDisplay =
-                                m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-                            var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-                            // console.log(hDisplay + " " + mDisplay + " " + sDisplay);
-                            return hDisplay + mDisplay + sDisplay;
-                        }
-                        entry[1].trackedSeconds = timer;
-                        entries.push(entry[1]);
-                    }
-                });
-                res.render("dashboard", {
-                    username: req.user.name,
-                    entries: entries
-                });
-                //console.log(entries[0].url);
-            } else {
-                res.render("dashboard", {
-                    username: req.user.name,
-                    entries: entries
-                });
+              let str = entry[0].split(".");
+              if (str[0] !== "www") {
+                entry[1].similarWebsite = "www." + entry[0];
+              } else {
+                entry[1].similarWebsite = entry[0];
+              }
+              let time_ = entry[1].trackedSeconds;
+              let timer = secondsToHms(time_);
+
+              function secondsToHms(d) {
+                d = Number(d);
+                var h = Math.floor(d / 3600);
+                var m = Math.floor((d % 3600) / 60);
+                var s = Math.floor((d % 3600) % 60);
+                var hDisplay =
+                  h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+                var mDisplay =
+                  m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+                var sDisplay =
+                  s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+                // console.log(hDisplay + " " + mDisplay + " " + sDisplay);
+                return hDisplay + mDisplay + sDisplay;
+              }
+              entry[1].trackedSeconds = timer;
+              entries.push(entry[1]);
             }
-        }).catch(e => {
-            console.log(e);
-        })
-    } catch (e) {
+          });
+          // res.render("dashboard", {
+          //     username: req.user.name,
+          //     entries: entries
+          // });
+          res.json({
+            success: true,
+            message: "user has successfully authenticated",
+            user: req.user,
+            data: entries
+          });
+          //res.send(entries);
+          //console.log(entries[0].url);
+        } else {
+          // res.render("dashboard", {
+          //     username: req.user.name,
+          //     entries: entries
+          // });
+          res.json({
+            success: true,
+            message: "user has successfully authenticated",
+            user: req.user,
+            data: entries
+          });
+          //res.json(req.user);
+        }
+      })
+      .catch(e => {
         console.log(e);
-    }
+      });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
--- routes/similarWebsite.js
+++ routes/similarWebsite.js
@@ -1,21 +1,21 @@
-const fetch = require('node-fetch');
-module.exports = async function (req, res) {
-    let url = req.originalUrl.split("?");
-    let similar = url[1].split("=");
-
-    let json = await getData(`http://13.235.90.61:5000/${similar[0]}/`)
-    res.render("similarWebsite", {
-        similar: similar[0],
-        json: json
-    });
-}
+const fetch = require("node-fetch");
+module.exports = async function(req, res) {
+  let url = req.originalUrl.split("?");
+  let similar = url[1].split("=");
+  console.log(similar[0]);
+  let json = await getData(`http://13.235.90.61:5000/${similar[0]}/`);
+  res.render("similarWebsite", {
+    similar: similar[0],
+    json: json
+  });
+};
 
 const getData = async url => {
-    try {
-        const response = await fetch(url);
-        const json = await response.json();
-        return json;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
 };
