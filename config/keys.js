d645278369133d3b128fd072d96696d9a020bafc
Praveen Sharma <sharmapraveenofficial@gmail.com>
2020-08-05 15:38:48 +0530

    keys changes

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,10 @@
 module.exports = {
-    googleAuth: {
-        clientID: "995926344319-p5eh5t9qgjh19oclosqiqi2mnmpgobit.apps.googleusercontent.com",
-        clientSecret: "GcExVRb7z5yKiEMb1egg4EZ6",
-        callbackURL: "http://localhost:3000/auth/google/callback"
-    },
-    mongoDb: "mongodb+srv://praveen_12:praveens12@cluster0-ruqab.mongodb.net/insights?retryWrites=true&w=majority"
-}
+  googleAuth: {
+    clientID:
+      "347756178893-hf40bn1hgsa1m0r6m1ngs3klo2j7lrfu.apps.googleusercontent.com",
+    clientSecret: "4cKefAHYrhM96O-6-F51ukIg",
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  mongoDb:
+    "mongodb+srv://praveen_12:praveens12@cluster0-ruqab.mongodb.net/insights?retryWrites=true&w=majority"
+};
